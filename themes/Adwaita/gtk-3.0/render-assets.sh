eeece7134dfd8ae3324f842c0feafb2e81391490
Jakub Steiner <jimmac@gmail.com>
2013-07-04 23:54:27 +0200

    consolidate asset rendering
    - "port" scale sliders to the new asset.svg
    - rename -new.* to the old names
    - add hidpi rendeirng of borders
    - render all

--- themes/Adwaita/gtk-3.0/render-assets.sh
+++ themes/Adwaita/gtk-3.0/render-assets.sh
@@ -3,7 +3,7 @@
 INKSCAPE="/usr/bin/inkscape"
 OPTIPNG="/usr/bin/optipng"
 
-SRC_FILE="assets.svg"
+SRC_FILE="assets-new.svg"
 ASSETS_DIR="assets"
 INDEX="assets.txt"
 
@@ -18,6 +18,17 @@ else
               --export-id-only \
               --export-png=$ASSETS_DIR/$i.png $SRC_FILE >/dev/null #\
     # && $OPTIPNG -o7 --quiet $ASSETS_DIR/$i.png 
+fi
+if [ -f $ASSETS_DIR/$i@2.png ]; then
+    echo $ASSETS_DIR/$i@2.png exists.
+else
+    echo
+    echo Rendering $ASSETS_DIR/$i@2.png
+    $INKSCAPE --export-id=$i \
+              --export-dpi=180 \
+              --export-id-only \
+              --export-png=$ASSETS_DIR/$i@2.png $SRC_FILE >/dev/null #\
+    # && $OPTIPNG -o7 --quiet $ASSETS_DIR/$i@2.png 
 fi
 done
 exit 0
